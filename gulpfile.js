c0d6b3c9477fdb19a621bb858561f05309281f61
Maurof18 <maurof809@gmail.com>
2023-10-19 13:51:24 -0300

    SASS y Creando la Primera Tarea

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
+const {src,dest} = require('gulp') //importa las dependencias
+//gulp-sass al ener una sola funcion no hace falta en encerrar sass con {}
+const sass = require('gulp-sass')(require('sass'))
 
-
+sass.compiler= require('dart-sass')
 function compilarSASS(){
-    console.log('compilando...');
+   //src es para encontrar la fuente de un archivo
+   //pipe para almacenar un archivo
+   //dest es una funcion para definir donde se va a almacenar un archivo
+   return src("./src/scss/app.scss")
+         .pipe( sass() )
+         .pipe( dest("./build/css") )
 }
 
 exports.compilarSASS = compilarSASS
